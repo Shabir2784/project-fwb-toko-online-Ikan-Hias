3a42e6603b706be49f4558503e3643e34bdd4fc1
shobir <shabir.a270804@gmail.com>
2025-07-11 16:50:03 +0800

    update

--- resources/views/penjual/profil.blade.php
+++ resources/views/penjual/profil.blade.php
@@ -33,7 +33,7 @@
         </div>
 
         <div class="mb-3">
-            <label>Nomor WhatsApp</label>
+            <label>No WhatsApp</label>
             <input type="text" name="no_wa" value="{{ old('no_wa', $pengguna->no_wa) }}" class="form-control">
         </div>
 
